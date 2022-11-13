b25b2cd7e9661f4b081c3bbb0741956e0b5e620f
jdussold <jdussold@gmail.com>
2022-11-13 16:38:34 -0600

    added deregister users endpoint

--- index.js
+++ index.js
@@ -336,6 +336,22 @@ app.post("/users", (req, res) => {
   });
 });
 
+//allow users to deregister
+app.delete("/users/:Username", (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + " was not found.");
+      } else {
+        res.status(200).send(req.params.Username + " was deleted.");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
 //Returns the API documentation
 app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
