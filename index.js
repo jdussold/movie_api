2bdc9ded01b7b1f7fd858516bd03bf31e661f7cf
jdussold <jdussold@gmail.com>
2022-11-20 16:07:40 -0600

    updated app.listen function

--- index.js
+++ index.js
@@ -390,6 +390,7 @@ app.use((error, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log("listening on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port " + port);
 });
