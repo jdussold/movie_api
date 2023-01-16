781a3dbb34979073890e7a400d688317c0ce7c74
jdussold <jdussold@gmail.com>
2023-01-15 20:33:47 -0600

    Updated CORS

--- index.js
+++ index.js
@@ -26,26 +26,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); //bodyParser middleware function
 app.use(morgan("common"));
 app.use(express.static("public"));
-app.use(cors()); //allow requests from all origins
+//app.use(cors()); //allow requests from all origins
 
-// //allow only certain origins to be given access
-// let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+//allow only certain origins to be given access
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+  "https://myflix-jd.netlify.app",
+];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         // If a specific origin isn’t found on the list of allowed origins
-//         let message =
-//           "The CORS policy for this application doesn’t allow access from origin " +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 let auth = require("./auth")(app);
 const passport = require("passport");
