c5c82a3f0d06d12d98d8a96e80822a524c4fd2da
jdussold <jdussold@gmail.com>
2023-05-06 21:27:06 -0500

    added new console log to featured movie endpoint for testing

--- index.js
+++ index.js
@@ -164,13 +164,14 @@ app.get(
 );
 
 //Get featured movies
+// Get featured movies
 app.get(
   "/movies/featured",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Movie.aggregate([{ $match: { Featured: true } }, { $sample: { size: 5 } }])
       .then((movies) => {
-        console.log("Featured movies:", movies);
+        console.log("Aggregation query results:", movies); // Add the logging statement here
         res.status(200).send(movies);
       })
       .catch((error) => res.status(500).send(`Error: ${error}`));
